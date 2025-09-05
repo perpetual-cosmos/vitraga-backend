6e1226ef740bfc8a210eb48b18fbf1943c508714
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-09-05 23:29:54 +0530

    debug

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY,
   console.log("Supabase URL exists:", !!process.env.SUPABASE_URL),
-console.log("Supabase Key exists:", !!process.env.SUPABASE_KEY)
+console.log("Supabase Key exists:", !!process.env.SUPABASE_SERVICE_KEY)
 );
 
 // Resend setup
